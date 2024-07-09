2574ceb33d60a0ca9968dd6634b6969edb252a23
S1owDi3 <MicroDimik64@mail.ru>
2024-07-10 00:09:32 +0400

    Update test_sender_stand_request.py

--- test_sender_stand_request.py
+++ test_sender_stand_request.py
@@ -3,13 +3,13 @@ import requests
 import configuration
 import data
 
-# создание заказа
+# Создание заказа
 def post_new_order(order_body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
                          headers=data.headers,
                          json=order_body)
 
-# получение заказа по номеру трекера
+# Получение номера заказа
 def get_order_track(track):
     return requests.get(configuration.URL_SERVICE + configuration.CREATE_ORDER + '?t=' + str(track),
                         headers=data.headers)
